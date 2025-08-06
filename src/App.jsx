64b727ba123fffecfcee80e8dd3238e72539e351
Nobuhito OKADA <okada@technoface.co.jp>
2025-08-07 08:29:04 +0900

    tmp

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,39 @@ import './App.css'
 
 function App() {
   const [value, setValue] = useState("")
+  const [results, setResults] = useState([])
+
+  function inputData(value) {
+    setValue(value);
+    var arr = [];
+    var water = Number(value) * 3;
+    for(var i = 1; i <= 5; i++) {
+      arr.push(`${water} x ${i} = ${(water * i).toFixed(2)} `)
+    }
+    setResults(arr);
+  }
 
   return (
     <>
-      <h1>豆計算</h1>
-      <div className="card">
-        <input value={value} onChange={function(e) { setValue(e.target.value) }}/>
-        <button onClick={function() {setValue("")}}>クリア</button>
+      <div className="calculator">
+          <div className="input-group">
+              <input type="number" step="0.01" value={value} onChange={inputData(value)}/>
+              <button onClick={function() {setValue("")}}>クリア</button>
+          </div>
+          
+          <div className="result" id="result">
+            <div className="card">
+              <table>
+                {results.map(result => (
+                  <tr>
+                    <td>{result}</td>
+                  </tr>
+                ))}
+              </table>
+            </div>            
+          </div>
       </div>
-      <div>
-        4.69 × {value} = {(4.69 * Number(value)).toFixed(2)}
-      </div>
+
     </>
   )
 }
